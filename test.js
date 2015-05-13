2ddc4b123ab461d71250e199b869face2820ad7f
FirstTimeInForever <doctortheswat@gmail.com>
2015-05-13 16:30:09 +0400

    Dam son...

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ document.title="JavaScript practice";
 var targetDiv=document.getElementById("test_show_div");
 for(var i=0;i<10;i++)
 {
-	targetDiv.textContent+="new stuff\n\n\n\n\n\n\n";
+	document.write("dam son\n");
 }
