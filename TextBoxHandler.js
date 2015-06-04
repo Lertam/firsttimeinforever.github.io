29319edeb6c62941f1ace00678c6bb10c0a24a63
FirstTimeInForever <doctortheswat@gmail.com>
2015-06-04 21:33:02 +0300

    Delete TextBoxHandler.js

--- TextBoxHandler.js
+++
@@ -1,6 +0,0 @@
-var targetTextBox=document.getElementById("inputStr");
-targetTextBox.addEventListener("change",function()
-{
-	var targetOutupText=document.getElementById("outputText");
-	targetOutupText.value=targetTextBox.value;
-});
